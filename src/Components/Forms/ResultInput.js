acadc2ecb0468ea63fe5ac0d9e21af7c3f9ae0f5
Mary Hanstad <mhanstad@nd.edu>
2024-03-06 18:39:59 -0500

    Updated with parse data - in the works

--- src/Components/Forms/ResultInput.js
+++ src/Components/Forms/ResultInput.js
@@ -1,8 +1,8 @@
-const ResultInput = ({ dictResult, inputResult, getResult, inputWord }) => {
+const ResultInput = ({ beeSolutions, inputWord, getResult }) => {
     return (
         <div>
             <h5>The word you inputted: {inputWord}</h5>
-            <h5>{getResult(dictResult, inputResult)}</h5>
+            <h5>{getResult(beeSolutions, inputWord)}</h5>
         </div>
     );
 };
