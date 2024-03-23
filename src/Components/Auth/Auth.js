8df3cd454f95130acbcbc9b2f5a6abda65430d75
Mary Hanstad <mhanstad@nd.edu>
2024-03-23 18:33:29 -0400

    Authentication Module draft

---
+++ src/Components/Auth/Auth.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { checkUser } from "./AuthService";
+
+/* Render Register and Login Modules */
+const AuthModule = () => {
+  const navigate = useNavigate();
+
+  // redirect already authenticated users back to home
+  useEffect(() => {
+    if (checkUser()) {
+      alert("You are already logged in");
+      navigate("/");
+    }
+  }, [navigate]);
+
+  return (
+    <div>
+      <Link to="/auth/register">
+        <button>Register</button>
+      </Link>
+      <br />
+      <br />
+      <Link to="/auth/login">
+        <button>Login</button>
+      </Link>
+    </div>
+  );
+};
+
+export default AuthModule;
